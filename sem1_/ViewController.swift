5ec17fb76594c03c14cf5cec3371ca2533d62197
AKSUMRUS <shimorhershow@gmail.com>
2023-09-08 20:30:31 +0300

    Initial Commit

---
+++ sem1_/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sem1_
+//
+//  Created by Paul Nikulshin on 08.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
